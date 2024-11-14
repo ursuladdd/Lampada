33d5b4cdc57b2a19a296df101628ec30e9c84c9f
ursuladdd <anaclarasugajimin14@gmail.com>
2024-11-14 17:06:03 -0300

    Começado tudo de novo

--- lâmpada.js
+++ lâmpada.js
@@ -1,1 +1,11 @@
+let ligada = document.getElementById('but-1');
+let desligada = document.getElementById('but-2');
+let imagem = document.getElementById('lp-fun');
 
+function lamLig() {
+    imagem.src = "./img/ligada.jpg"
+}
+
+
+
+ligada.addEventListener("click", lamlig)
