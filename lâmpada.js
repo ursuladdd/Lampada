f0dab9707e43c9fd1610a06399f85e7aeec1cd04
ursuladdd <anaclarasugajimin14@gmail.com>
2024-11-14 17:32:47 -0300

    Acabou finalmente

--- lâmpada.js
+++ lâmpada.js
@@ -3,7 +3,7 @@ let desligada = document.getElementById('but-2');
 let imagem = document.getElementById('lp-fun');
 
 function verificaLamp() {
-    return imagem.src.indexOf = ("quebrada") > -1
+    return imagem.src.indexOf ('quebrada') > -1
 }
 
 function lamLig() {
@@ -29,4 +29,4 @@ ligada.addEventListener("click", lamLig)
 desligada.addEventListener("click", lamDes)
 imagem.addEventListener ( 'mouseover', lamLig );
 imagem.addEventListener ( 'mouseleave', lamDes );
-imagem.addEventListener ("ondblclick", duplo);
+imagem.addEventListener ("dblclick", duplo)
