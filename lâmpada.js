8883c8147aaae1dad2ee346df7862f94cf4af9a7
ursuladdd <anaclarasugajimin14@gmail.com>
2024-11-14 17:28:12 -0300

    Estressee

--- lâmpada.js
+++ lâmpada.js
@@ -2,10 +2,31 @@ let ligada = document.getElementById('but-1');
 let desligada = document.getElementById('but-2');
 let imagem = document.getElementById('lp-fun');
 
-function lamLig() {
-    imagem.src = "./img/ligada.jpg"
+function verificaLamp() {
+    return imagem.src.indexOf = ("quebrada") > -1
 }
 
+function lamLig() {
+    if (!verificaLamp ()) {
+        imagem.src = "./img/ligada.jpg"
+    }
+    
+}
 
+function lamDes() {
+    if (!verificaLamp()) {
+        imagem.src = "./img/desligada.jpg"
+    }
+    
+}
 
-ligada.addEventListener("click", lamlig)
+function duplo() {
+    imagem.src = "./img/quebrada.jpg"
+    
+}
+
+ligada.addEventListener("click", lamLig)
+desligada.addEventListener("click", lamDes)
+imagem.addEventListener ( 'mouseover', lamLig );
+imagem.addEventListener ( 'mouseleave', lamDes );
+imagem.addEventListener ("ondblclick", duplo);
