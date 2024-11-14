29e86f584cac51721cbd44f0d2a941fdc482aafe
ursuladdd <anaclarasugajimin14@gmail.com>
2024-11-14 16:56:31 -0300

    Refazendo tudo

--- lâmpada.js
+++ lâmpada.js
@@ -1,15 +1,1 @@
-function ligar() {
-    let imagem = document.getElementById('lp-fun');
-    imagem.src = "./img/ligada.jpg";
-}
 
-function desligar() {
-    let imagem = document.getElementById('lp-fun');
-    imagem.src = "./img/desligada.jpg";
-}
-
-
-function duplo() {
-    let imagem = document.getElementById('lp-fun');
-    imagem.src = "./img/quebrada.jpg";
-}
