7ed9a05b2a63e82c74959d599424cfc6d6b62c69
ursuladdd <anaclarasugajimin14@gmail.com>
2024-11-14 09:30:34 -0300

    Colocando os comados

---
+++ lâmpada.js
@@ -0,0 +1,15 @@
+function ligar() {
+    let imagem = document.getElementById('lp-fun');
+    imagem.src = "./img/ligada.jpg";
+}
+
+function desligar() {
+    let imagem = document.getElementById('lp-fun');
+    imagem.src = "./img/desligada.jpg";
+}
+
+
+function duplo() {
+    let imagem = document.getElementById('lp-fun');
+    imagem.src = "./img/quebrada.jpg";
+}
